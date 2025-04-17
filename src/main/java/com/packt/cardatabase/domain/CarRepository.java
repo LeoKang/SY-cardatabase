1894702e35e5a1862922380c2f456dfa30abfbbe
Seungjin in SY <xeungjin@gmail.com>
2025-04-17 16:15:37 +0900

    CarRepository 추가

--- src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -1,6 +1,17 @@
 package com.packt.cardatabase.domain;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
 public interface CarRepository extends CrudRepository<Car, Long> {
+
+    List<Car> findByBrand(String brand);
+    List<Car> findByColor(String color);
+    List<Car> findByModelYear(int modelYear);
+
+    List<Car> findByBrandAndModel(String brand, String model);
+    List<Car> findByBrandOrColor(String brand, String color);
+
+    List<Car> findByBrandOrderByModelYearAsc(String brand);
 }
