a45f82659ec7a35c44404915299e1bbf32a4fa2e
Seungjin in SY <xeungjin@gmail.com>
2025-04-24 11:15:58 +0900

    UserDetailsServiceImpl

---
+++ src/main/java/com/packt/cardatabase/domain/AppUser.java
@@ -0,0 +1,29 @@
+package com.packt.cardatabase.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
