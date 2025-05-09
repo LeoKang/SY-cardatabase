a6e2362187e59b0293fdeeb3f802c548a16de07e
Seungjin in SY <xeungjin@gmail.com>
2025-05-09 15:22:41 +0900

    jjwt 버전 업에 따른 워닝 메시지를 없애기 위한 코드 수정

--- src/main/java/com/packt/cardatabase/service/JwtService.java
+++ src/main/java/com/packt/cardatabase/service/JwtService.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
@@ -16,14 +18,23 @@ public class JwtService {
     static final long EXPIRATIONTIME = 86400000;
     static final String PREFIX = "Bearer";
 
-    static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+//    static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    static String secretString = "this_is_a_very_secure_256_bit_secret_key_123456";
+    static final SecretKey key
+            = new SecretKeySpec(secretString.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
 
     public String getToken(String username) {
+/*
         String token = Jwts.builder().setSubject(username)
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
                 .signWith(key).compact();
 
         return token;
+ */
+        return Jwts.builder()
+                .subject(username)
+                .signWith(key, Jwts.SIG.HS256)
+                .compact();
     }
 
     public String getAuthUser(HttpServletRequest request) {
